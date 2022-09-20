fcc3dfee32ac96795aee30460883d831b045cc73
eric.cheng <eric.cheng@psp-power.com.tw>
2022-09-20 12:22:32 +0800

    chore: modify before method to let method

--- spec/api/api_v0/posts_spec.rb
+++ spec/api/api_v0/posts_spec.rb
@@ -1,18 +1,22 @@
 describe ApiV0::Posts do
-  before(:all) do
-    @user = create(:user)
-    @posts = create_list(:post, 3, user: @user)
-    @access_token = create(:api_access_token, user: @user)
-  end
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
+  let(:api_access_token) { create(:api_access_token) }
+  
+  context "GET /api/v0/posts" do
+    let(:accesskey_params) { { access_key: api_access_token.key, user: user } }
+    let(:result) { JSON.parse(response.body) }
 
-  context 'GET /api/v0/posts' do
-    it 'should return 200 and posts' do
-      get '/api/v0/posts', params: { access_key: @access_token.key }
-
-      result = JSON.parse(response.body)
+    it "return 200 ok" do
+      get '/api/v0/posts', params: accesskey_params
 
       expect(response.status).to eq(200)
-      expect(result.size).to eq(@user.posts.size)
+    end
+
+    it "return posts" do
+      get "/api/v0/posts", params: accesskey_params 
+
+      expect(result.size).to eq(user.posts.size)
     end
   end
 end
