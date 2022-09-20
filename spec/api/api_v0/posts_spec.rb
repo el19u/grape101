481422ff8d04156283014bb86b98bbbd49830292
eric.cheng <eric.cheng@psp-power.com.tw>
2022-09-20 15:40:15 +0800

    test: test add new post api

--- spec/api/api_v0/posts_spec.rb
+++ spec/api/api_v0/posts_spec.rb
@@ -1,22 +1,35 @@
 describe ApiV0::Posts do
   let(:user) { create(:user) }
-  let(:post) { create(:post) }
+  let(:article) { create(:post) }
   let(:api_access_token) { create(:api_access_token) }
+  let(:access_key) { api_access_token.key }
+  let(:result) { JSON.parse(response.body) }
   
   context "GET /api/v0/posts" do
-    let(:accesskey_params) { { access_key: api_access_token.key, user: user } }
-    let(:result) { JSON.parse(response.body) }
+    let(:access_key_params) { { access_key: access_key, user: user } }
 
     it "return 200 ok" do
-      get '/api/v0/posts', params: accesskey_params
+      get '/api/v0/posts', params: access_key_params
 
       expect(response.status).to eq(200)
     end
 
     it "return posts" do
-      get "/api/v0/posts", params: accesskey_params 
+      get "/api/v0/posts", params: access_key_params 
 
       expect(result.size).to eq(user.posts.size)
+    end
+  end
+
+  context "Post /api/v0/posts" do
+    it "create new post" do
+      title = "title"
+      context = "context"
+      post "/api/v0/posts", params: { access_key: access_key, user: user, title: title, context: context }
+
+      expect(response.status).to eq(201)
+      expect(result["title"]).to eq(title)
+      expect(result["context"]).to eq(context)
     end
   end
 end
