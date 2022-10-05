98f79cf1909a7c5682b926aa656f4e1236253662
eric.cheng <eric.cheng@psp-power.com.tw>
2022-10-05 16:37:21 +0800

    modify: use subject to handle duplicate code

--- spec/api/api_v0/posts_spec.rb
+++ spec/api/api_v0/posts_spec.rb
@@ -4,70 +4,99 @@ describe ApiV0::Posts do
   let(:api_access_token) { create(:api_access_token, user: user) }
   let(:access_key) { api_access_token.key }
   let(:result) { JSON.parse(response.body) }
-  
+
   context "GET /api/v0/posts" do
+    subject { get "/api/v0/posts", params: { access_key: access_key } }
+
     it "return 200 ok" do
-      get '/api/v0/posts', params: { access_key: access_key }
+      subject
 
       expect(response.status).to eq(200)
     end
 
     it "return posts" do
-      get "/api/v0/posts", params: { access_key: access_key }
+      posts
+      subject
 
-      expect(result.map { |post| post["id"] }).to eq(posts.pluck(:id))
+      expect(result.map {|post| post["id"]}).to eq(posts.pluck(:id))
     end
   end
 
   context "Get /api/v0/posts/:id" do
-    it "return a post by id" do
-      post = posts.sample
-      post = create(:post, user: user)
+    let(:post) { posts.sample }
+    subject { get "/api/v0/posts/#{post.id}", params: { access_key: access_key } }
 
-      get "/api/v0/posts/#{post.id}", params: { access_key: access_key }
+    it "return 200 ok to show post" do
+      subject
 
       expect(response.status).to eq(200)
+    end
+
+    it "return a post by id" do
+      subject
+
       expect(result["title"]).to eq(post.title)
     end
   end
 
   context "Post /api/v0/posts" do
-    it "create new post" do
-      title = "title"
-      context = "context"
-      post "/api/v0/posts", params: { access_key: access_key, user: user, title: title, context: context }
+    subject { post "/api/v0/posts", params: { access_key: access_key, user: user, title: "title", context: "context" } }
+
+    it "retrun 201 to create a post" do
+      subject
 
       expect(response.status).to eq(201)
-      expect(result["title"]).to eq(title)
-      expect(result["context"]).to eq(context)
+    end
+
+    it "create new post object" do
+      subject
+
+      expect(result["title"]).to eq("title")
+      expect(result["context"]).to eq("context")
+    end
+
+    it "create new post" do
+      expect { subject }.to change { user.posts.count }.by(1)
     end
   end
 
   context "Patch /api/v0/posts/:id" do
-    it "update a post" do
-      post = posts.sample
-      title = "updated title"
-      context = "updated context"
+    let(:post) { posts.sample }
+    subject { patch "/api/v0/posts/#{post.id}", params: { access_key: access_key, user: user, title: "new_title", context: "new_context" }}
 
-      patch "/api/v0/posts/#{post.id}", params: { access_key: access_key, title: title, context: context }
-
+    it "return 200 to update a post" do
+      subject
       expect(response.status).to eq(200)
-      expect(result["title"]).to eq(title)
-      expect(result["context"]).to eq(context)
+    end
+
+    it "update a post" do
+      subject
+
+      expect{ post.reload }.to change { post.title }.from(post.title).to("new_title") &
+                               change { post.context }.from(post.context).to("new_context")
+
+      # post.reload
+
+      # expect(result["title"]).to eq(post.title)
+      # expect(result["context"]).to eq(post.context)
     end
   end
 
   context "Delete /api/v0/posts/:id" do
-    it "delete a post" do
-      post = posts.sample
+    let(:post) { posts.sample }
+    subject { delete "/api/v0/posts/#{post.id}", params: { access_key: access_key } }
 
-      delete "/api/v0/posts/#{post.id}", params: { access_key: access_key }
-
+    it "return 200 to delete post" do
+      subject
       expect(response.status).to eq(200)
+    end
+
+    it "delete a post" do
+      subject
+
       expect(result["title"]).to eq(post.title)
       expect(result["context"]).to eq(post.context)
-      expect{Post.find(post.id)}.to (raise_error ActiveRecord::RecordNotFound)
-      expect(post.destroyed?) === true
+      expect{ post.reload }.to raise_error ActiveRecord::RecordNotFound
     end
   end
 end
